27dc71ff36821e500847e3b61965ed6753d3c860
Erik Vergara <evergara@idtolu.com.co>
2023-03-06 16:34:32 -0500

    Clase31

---
+++ Clase31_ExprecionesRegulares.js
@@ -0,0 +1,16 @@
+/**
+ * 
+    ______                          _                                                __                    
+   / ____/  ______  ________  _____(_)___  ____  ___  _____   ________  ____ ___  __/ /___ _________  _____
+  / __/ | |/_/ __ \/ ___/ _ \/ ___/ / __ \/ __ \/ _ \/ ___/  / ___/ _ \/ __ `/ / / / / __ `/ ___/ _ \/ ___/
+ / /____>  </ /_/ / /  /  __/ /__/ / /_/ / / / /  __(__  )  / /  /  __/ /_/ / /_/ / / /_/ / /  /  __(__  ) 
+/_____/_/|_/ .___/_/   \___/\___/_/\____/_/ /_/\___/____/  /_/   \___/\__, /\__,_/_/\__,_/_/   \___/____/  
+          /_/                                                        /____/                                
+          */
+
+/**
+ * Las expresiones regulares son patrones que se utilizan para hacer coincidir combinaciones de caracteres en cadenas.
+ * En JavaScript, las expresiones regulares también son objetos.
+ * Estos patrones se utilizan con los métodos exec() y test() de RegExp, y con match(), matchAll(), replace(), replaceAll() (en-US), search() y split() métodos de String.
+ *
+ */
